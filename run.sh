83c44e84405c93bade1a13ec1e7ed4f6db3e0b17
Ki Yung Ahn <kyagrd@gmail.com>
2020-05-30 22:12:51 +0900

    image minor update

--- run.sh
+++ run.sh
@@ -2,16 +2,7 @@ LAB="--env JUPYTER_ENABLE_LAB=yes"
 TAG=picalc
 HOSTPORT=8888
 
-if [ -z "$(ls -A ".snapshots" 2>/dev/null)" ]; then
-	docker run --rm -p $HOSTPORT:8888 $LAB \
-		-v "$PWD":/home/jovyan/picalc \
-		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
-	       	kyagrd/ihaskell:$TAG
-else
-	docker run --rm -p $HOSTPORT:8888 $LAB \
-		-v "$PWD":/home/jovyan/picalc \
-	       	-v "$PWD/.snapshots":/opt/stack/snapshots \
-	       	-v "$PWD/.stack-work":/opt/stack/global-project/.stack-work \
-		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
-	       	kyagrd/ihaskell:$TAG
-fi
+docker run --rm -p $HOSTPORT:8888 $LAB \
+	-v "$PWD":/home/jovyan/picalc \
+	--env JUPYTER_TOKEN=x --name ihaskell_notebook \
+       	kyagrd/ihaskell:$TAG
